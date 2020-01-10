5769dcd489cf91b2935892a1b8627a08df6888f0
leandro venezia <dev.venezia@gmail.com>
2020-01-10 11:44:49 -0300

    edit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/list-client-crud-angular'));
+app.use(express.static('./dist/list-client-form-reactive'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/list-client-crud-angular/index.html'));
+res.sendFile(path.join(__dirname,'/dist/list-client-form-reactive/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
